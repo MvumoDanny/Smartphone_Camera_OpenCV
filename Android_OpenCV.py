1baa506f52b8d50098d0b9ac878b156b09e17999
MvumoDanny <40689845+MvumoDanny@users.noreply.github.com>
2019-06-17 14:44:12 +0300

    Add files via upload

---
+++ Android_OpenCV.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+import os
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+
+vcap = cv2.VideoCapture("rtsp://192.168.1.2:5554/camera", cv2.CAP_FFMPEG)
+while(1):
+
+ret, frame = vcap.read()
+    if ret == False:
+        print("Frame is empty")
+        break;
+    else:
+        cv2.imshow('VIDEO', frame)
+        cv2.waitKey(1)
+//https://medium.com/beesightsoft/opencv-python-connect-to-android-camera-via-rstp-9eb78e2903d5
+//Change “192.168.1.2” to your android device address
